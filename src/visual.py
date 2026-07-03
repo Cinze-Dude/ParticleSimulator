fc68059b0fc2e558fca2466e2054a58f109507aa
Cinze-Dude <hebaalnady88@gmail.com>
2026-07-03 18:13:53 +0300

    Fixed the debug mode of visual.py

--- src/visual.py
+++ src/visual.py
@@ -1,10 +1,28 @@
-import json
 import sys
+import json
+import turtle
+
+screen = turtle.Screen()
+screen.tracer(0)
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
 
 for line in sys.stdin:
-    print("RAW:", line)
     try:
-        obj = json.loads(line)
-        print("Parsed:", obj)
-    except Exception as e:
-        print("JSON error:", e)
+        frame = json.loads(line)
+    except:
+        continue
+
+    t.clear()
+
+    for p in frame["particles"]:
+        x, y, z = p["pos"]
+        t.goto(x * 100, y * 100)
+        print(p["pos"][x])
+        t.dot(10)
+
+    screen.update()
+
+screen.mainloop()
